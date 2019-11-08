39a123522af2c0ff424e3d6556f6034a65b5d97b
Touraj Ostovari <tornet656@gmail.com>
2019-11-08 14:42:41 +0330

    Add project files.

---
+++ DDA_Transform_Line/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace DDA_Transform_Line
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Source_X = new System.Windows.Forms.TextBox();
+            this.Source_Y = new System.Windows.Forms.TextBox();
+            this.Tx = new System.Windows.Forms.TextBox();
+            this.Ty = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.Dest_X = new System.Windows.Forms.TextBox();
+            this.Dest_Y = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Dest_Y);
+            this.groupBox1.Controls.Add(this.Dest_X);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.Source_Y);
+            this.groupBox1.Controls.Add(this.Source_X);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(549, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(256, 285);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Draw DDA Line:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label10);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.Ty);
+            this.groupBox2.Controls.Add(this.Tx);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(549, 297);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(256, 190);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Transform:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "X:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Y:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Tx:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 99);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Ty:";
+            // 
+            // Source_X
+            // 
+            this.Source_X.Location = new System.Drawing.Point(10, 47);
+            this.Source_X.Name = "Source_X";
+            this.Source_X.Size = new System.Drawing.Size(120, 20);
+            this.Source_X.TabIndex = 2;
+            // 
+            // Source_Y
+            // 
+            this.Source_Y.Location = new System.Drawing.Point(10, 119);
+            this.Source_Y.Name = "Source_Y";
+            this.Source_Y.Size = new System.Drawing.Size(120, 20);
+            this.Source_Y.TabIndex = 3;
+            // 
+            // Tx
+            // 
+            this.Tx.Location = new System.Drawing.Point(10, 64);
+            this.Tx.Name = "Tx";
+            this.Tx.Size = new System.Drawing.Size(120, 20);
+            this.Tx.TabIndex = 2;
+            // 
+            // Ty
+            // 
+            this.Ty.Location = new System.Drawing.Point(10, 127);
+            this.Ty.Name = "Ty";
+            this.Ty.Size = new System.Drawing.Size(120, 20);
+            this.Ty.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(148, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 222);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Draw ...";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Turquoise;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(148, 51);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 109);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "do Transfer job ...";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label5.Location = new System.Drawing.Point(5, 160);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(125, 10);
+            this.label5.TabIndex = 5;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(45, 20);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = ":Source:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(48, 174);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(66, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = ":Destination:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(10, 185);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(17, 13);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "X:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(10, 227);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(17, 13);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "Y:";
+            // 
+            // Dest_X
+            // 
+            this.Dest_X.Location = new System.Drawing.Point(10, 202);
+            this.Dest_X.Name = "Dest_X";
+            this.Dest_X.Size = new System.Drawing.Size(120, 20);
+            this.Dest_X.TabIndex = 10;
+            // 
+            // Dest_Y
+            // 
+            this.Dest_Y.Location = new System.Drawing.Point(10, 244);
+            this.Dest_Y.Name = "Dest_Y";
+            this.Dest_Y.Size = new System.Drawing.Size(120, 20);
+            this.Dest_Y.TabIndex = 11;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.ForeColor = System.Drawing.Color.Red;
+            this.label10.Location = new System.Drawing.Point(16, 163);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(222, 13);
+            this.label10.TabIndex = 5;
+            this.label10.Text = "Only Integer - Decimal values are expected ...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(817, 499);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox Source_Y;
+        private System.Windows.Forms.TextBox Source_X;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox Ty;
+        private System.Windows.Forms.TextBox Tx;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox Dest_Y;
+        private System.Windows.Forms.TextBox Dest_X;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ DDA_Transform_Line/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DDA_Transform_Line
+{
+
+    public partial class Form1 : Form
+    {
+        public static int y2 = 60; // x2
+        public static float y1 = 30.0f; // x1
+                                        //==============
+        public static int x2 = 80; // y2
+        public static float x1 = 50; // y1
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Developed by Mr. Touraj Ostovari || 2019 Nov FOR UNIVERSITY LESSON :: Algorithms of computer's graphic";
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+            try
+            {
+                Graphics graphics = e.Graphics;
+                Pen pen = new Pen(System.Drawing.Color.Black, 5.0f);
+
+
+                //==============
+                float rep_x1 = x1;
+                float rep_y1 = y1;
+                System.Collections.Generic.List<float> collect = new List<float>();
+
+
+                System.Collections.Generic.Dictionary<int, int> pairs = new Dictionary<int, int>();
+
+                float m = float.Parse(((float.Parse((y2 - y1).ToString()) / float.Parse((x2 - x1).ToString())).ToString("f3")));
+                if (m > 1)
+                {
+                    while ((x1 <= x2 || x1 != x2) && (y1 != y2))
+                    {
+                        collect.Add(x1);
+                        x1 += m;
+                        y1 += 1.0f;
+                    }
+                    for (float i = rep_y1; i <= y2;)
+                    {
+                        try
+                        {
+                            graphics.DrawLine(pen, i++, float.Parse((Math.Round(collect[(int)i])).ToString()), i, float.Parse((Math.Round(collect[(int)i])).ToString()));
+                        }
+                        catch (Exception)
+                        { }
+                    }
+                }
+                else if (m < 1)
+                {
+                    while ((x1 != x2) && (y1 <= y2 || y1 != y2))
+                    {
+                        collect.Add(y1);
+                        y1 += m;
+                        x1 += 1;
+                    }
+                    for (float i = rep_x1; i <= x2;)
+                    {
+                        try
+                        {
+                            graphics.DrawLine(pen, i++, float.Parse((Math.Round(collect[(int)i])).ToString()), i, float.Parse((Math.Round(collect[(int)i])).ToString()));
+                        }
+                        catch (Exception)
+                        { }
+                    }
+                }
+                else if (m == 1)
+                {
+                    graphics.DrawLine(pen, x1, y1, x2, y2);
+                }
+                graphics.Dispose();
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(!String.IsNullOrWhiteSpace(Source_X.Text) && !String.IsNullOrWhiteSpace(Source_Y.Text) && !String.IsNullOrWhiteSpace(Dest_X.Text) && !String.IsNullOrWhiteSpace(Dest_Y.Text))
+            {
+                x1 = int.Parse(Source_X.Text);
+                y2 = int.Parse(Source_Y.Text);
+                x2 = int.Parse(Dest_X.Text);
+                y2 = int.Parse(Dest_Y.Text);
+            }
+            this.Invalidate();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+            int tempX = int.Parse(Tx.Text); int tempY = int.Parse(Ty.Text);
+            x1  = (x1 + (tempX)); y1 = (y1 + (tempY));
+            x2 = (x2 + (tempX)); y2 = (y2 + (tempY));
+            }
+            catch (Exception)
+            {
+
+            }
+            finally {this.Invalidate(); }
+            
+        }
+    }
+}
